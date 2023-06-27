fdcf301a4942dbf4af9ecc1cebbc1764161399b5
mnr73 <mnrmilad@gmail.com>
2023-06-28 02:00:14 +0330

    change header name

--- src/layouts/Mafia.vue
+++ src/layouts/Mafia.vue
@@ -1,6 +1,6 @@
 <template>
-	<header class="bg-red-500 h-12 flex fixed w-full">
-		<h1 class="w-full flex justify-center items-center text-white font-bold">mafia</h1>
+	<header class="bg-red-500 h-12 flex fixed w-full z-50">
+		<h1 class="w-full flex justify-center items-center text-white font-bold">مافیا</h1>
 	</header>
 	<div class="bg-gray-50 min-h-screen pt-12 xl:container mx-auto">
 		<slot></slot>
