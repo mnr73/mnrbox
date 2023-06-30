7544ad575cab50e59d61814ec5b36246d7dd8383
mnr73 <mnrmilad@gmail.com>
2023-06-30 18:51:26 +0330

    add game steps

--- src/pages/Mafia/AssignRoles.vue
+++ src/pages/Mafia/AssignRoles.vue
@@ -44,7 +44,7 @@ const openUser = computed(() => {
 			<h2 class="font-bold">تقسیم نقش ها</h2>
 		</div>
 		<div class="flex flex-wrap gap-2 justify-center">
-			<div class="bg-white p-3 w-24 rounded-md border-t-4 flex-grow text-center "
+			<div class="bg-white p-3 w-24 rounded-md border-t-4 flex-grow text-center cursor-pointer"
 				:class="{ 'opacity-70': user.seen, 'border-sky-500 shadow-md': !user.seen }"
 				v-for="user in _.sortBy(users, 'seen')" :key="user.id" @click="user.open = true">
 				<div class="text-lg">{{ user.name }}</div>
@@ -52,7 +52,8 @@ const openUser = computed(() => {
 		</div>
 		<div v-show="openUser" @click.self="openUser.seen = true; openUser.open = false"
 			class="fixed w-full h-screen bg-black bg-opacity-30 top-0 left-0 z-50 flex items-center justify-center">
-			<div class="w-11/12 h-2/6 bg-white shadow-lg rounded-md flex flex-col gap-10 items-center justify-center">
+			<div
+				class="w-11/12 h-2/6 max-w-screen-sm bg-white shadow-lg rounded-md flex flex-col gap-10 items-center justify-center">
 				<div class="">{{ openUser?.name }}</div>
 				<div class="">{{ _.find(roles, ['userId', openUser?.id])?.roleName }}</div>
 				<button class="bg-red-600 text-white p-2 rounded-md" @click="openUser.seen = true; openUser.open = false">
--- src/pages/Mafia/Game.vue
+++ src/pages/Mafia/Game.vue
@@ -2,71 +2,159 @@
 import MnrCheckSlider from "@/components/mnr/MnrCheckSlider.vue";
 import MnrNumber from "@/components/mnr/MnrNumber.vue";
 import { Icon } from "@iconify/vue";
-import { ref, watch, computed, onMounted } from "vue";
+import { ref, watch, computed, onMounted, reactive } from "vue";
 import mafia from "@/modules/mafia";
 import MnrSelect from "@/components/mnr/MnrSelect.vue";
 import _ from "lodash";
 import Bottom from "@/components/Bottom.vue";
 
 const data = new mafia;
+const daysBox = ref(null);
 const users = ref();
 const roles = ref();
+const rounds = ref([]);
+const selectedIndex = ref();
+const game = reactive({
+	zeroNight: null,
+	rounds: rounds.value,
+});
 // let selectedUser = ref()
 
 users.value = data.getActiveUsers();
-roles.value = data.getPlayers();
+roles.value = data.getPlayers([]);
 
+let rounds_structure = {
+	stepNumber: 0,
+	steps: {
+		night: {
+			number: 0,
+			type: 'night',
+			active: true,
+			name: "شب",
+			target_by: {}
+		},
+		day: {
+			number: 1,
+			type: 'day',
+			active: false,
+			name: "روز",
+			target_by: {}
+		},
+		vote_1: {
+			number: 2,
+			type: 'vote_1',
+			active: false,
+			name: "رای 1",
+			votes: {},
+		},
+		defense: {
+			number: 3,
+			type: 'defense',
+			active: false,
+			name: "دفاع",
+			target_by: {},
+		},
+		vote_2: {
+			number: 4,
+			type: 'vote_2',
+			active: false,
+			name: "رای 2",
+			votes: {},
+		}
+	}
 
+};
+
+if (_.find(roles.value, ['class', 'ghazi'])) {
+	rounds_structure.steps.ghazi = {
+		number: _.max(_.map(rounds_structure.steps, 'number')) + 1,
+		type: 'ghazi',
+		active: false,
+		name: "قاضی",
+		do: null,
+		role: null
+	}
+}
+if (_.find(roles.value, ['class', 'shahrdar'])) {
+	rounds_structure.steps.shahrdar = {
+		number: _.max(_.map(rounds_structure.steps, 'number')) + 1,
+		type: 'shahrdar',
+		active: false,
+		name: "شهردار",
+		veto: null
+	}
+}
+
+onMounted(() => {
+	rounds.value.push(_.cloneDeep(rounds_structure))
+	selectedIndex.value = rounds.value.length - 1
+	// game.value.rounds = rounds.value
+})
+
+const selectedRound = computed(() => {
+	return rounds.value[selectedIndex.value]
+})
+
+function nextStep() {
+	if (selectedIndex.value == 0) {
+		if (selectedRound.value.stepNumber === 0) {
+			selectedRound.value.stepNumber = 1
+			selectedRound.value.steps.day.active = true
+			return;
+		}
+	} else {
+		let maxNumber = _.max(_.map(selectedRound.value.steps, 'number'));
+		if (selectedRound.value.stepNumber < maxNumber) {
+			selectedRound.value.stepNumber++
+			_.find(selectedRound.value.steps, ['number', selectedRound.value.stepNumber]).active = true
+			return;
+		}
+	}
+	rounds.value.push(_.cloneDeep(rounds_structure))
+	selectedIndex.value++
+	daysBox.value.scrollLeft = -10000
+}
 
 </script>
 <template>
-	<div class="overflow-x-auto sm:my-3 my-2 sm:px-5 px-2">
+	<div class="overflow-x-auto sm:my-3 my-2 sm:px-5 px-2 text-sm" ref="daysBox">
 		<div class="flex gap-2">
-			<div class="bg-slate-800 text-white h-8 px-2 rounded-full flex items-center justify-center flex-shrink-0"
-				style="min-width: 2rem;">1
+			<div class="bg-slate-700 text-white h-8 px-2 rounded-full flex items-center justify-center flex-shrink-0"
+				style="min-width: 2rem;" :class="{ 'bg-sky-600': selectedIndex == index }" v-for="(round, index) in rounds"
+				:key="index">{{ index || 'معارفه' }}
 			</div>
-			<div class="bg-slate-800 text-white h-8 px-2 rounded-full flex items-center justify-center flex-shrink-0"
-				style="min-width: 2rem;">2
-			</div>
-			<div class="bg-slate-800 text-white h-8 px-2 rounded-full flex items-center justify-center flex-shrink-0"
-				style="min-width: 2rem;">3
-			</div>
-			<div class="bg-slate-800 text-white h-8 px-2 rounded-full flex items-center justify-center flex-shrink-0"
-				style="min-width: 2rem;">4
-			</div>
-			<div class="w-1 flex-shrink-0"></div>
+			<div class="w-10 flex-shrink-0"></div>
 		</div>
 	</div>
-	<div class="overflow-x-auto sm:my-3 my-2 sm:px-5 px-2">
+	<div class="overflow-x-auto sm:my-3 my-2 sm:px-5 px-2 text-sm">
 		<div class="flex gap-2 font-light">
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				شب
-			</div>
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				روز
-			</div>
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				رای اول
-			</div>
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				رای دوم
-			</div>
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				قاضی
-			</div>
-			<div class="bg-slate-800 text-white px-3 py-1 rounded-full flex items-center justify-center flex-shrink-0">
-				شهردار
-			</div>
+			<template v-for="(round, key) in selectedRound?.steps" :key="key">
+				<div class="bg-slate-400 text-white px-2 py-1 rounded-full flex items-center justify-center flex-shrink-0"
+					:class="{ '!bg-sky-600': selectedRound.stepNumber == round.number, '!bg-slate-800': round.active && selectedRound.stepNumber != round.number }"
+					v-if="selectedIndex != 0 || round.number < 2">
+					{{ round.name }}
+				</div>
+			</template>
 			<div class="w-1 flex-shrink-0"></div>
 		</div>
 	</div>
 	<div class="sm:p-5 p-2">
-		<Bottom>
-			<div class="p-2 border-t flex h-16 gap-2">
-				<button class="bg-slate-700 text-white shadow-md p-2 w-3/12 rounded-2xl">موزیک</button>
-				<button class="bg-slate-700 text-white shadow-md p-2 w-3/12 rounded-2xl">تایمر</button>
-				<button class="bg-slate-700 text-white shadow-md p-2 w-6/12 rounded-2xl">مرحله بعد</button>
+		<div v-if="selectedIndex == 0 && selectedRound.stepNumber == 0">
+			<div class="p-4">در بعضی از سناریو ها تیم مافیا در شب قبل از معارفه بیدار شده و یک‌دیگر را می‌شناسند.</div>
+			<div class="flex gap-5 justify-center">
+				<button class="bg-slate-200 shadow-lg p-2 rounded-md"
+					:class="{ 'bg-sky-500 text-white': game.zeroNight === true }" @click="game.zeroNight = true">شناختند</button>
+				<button class="bg-slate-200 shadow-lg p-2 rounded-md"
+					:class="{ 'bg-sky-500 text-white': game.zeroNight === false }" @click="game.zeroNight = false">لازم
+					نیست</button>
 			</div>
-		</Bottom>
+		</div>
 	</div>
+	<Bottom>
+		<div class="p-2 border-t flex h-16 gap-2">
+			<button class="bg-slate-700 text-white shadow-md p-2 w-3/12 rounded-2xl">موزیک</button>
+			<button class="bg-slate-700 text-white shadow-md p-2 w-3/12 rounded-2xl">تایمر</button>
+			<button class="bg-slate-700 text-white shadow-md p-2 w-6/12 rounded-2xl" @click="nextStep">مرحله بعد</button>
+		</div>
+	</Bottom>
 </template>
