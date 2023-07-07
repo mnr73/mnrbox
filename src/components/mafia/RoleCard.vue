c24f158dc17e4b14fc2fa20a6229241194717b06
mnr73 <mnrmilad@gmail.com>
2023-07-07 14:16:30 +0330

    add icon to role card and change formatter

--- src/components/mafia/RoleCard.vue
+++ src/components/mafia/RoleCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import MnrCheckSlider from '@/components/mnr/MnrCheckSlider.vue';
+import { Icon } from '@iconify/vue';
 
 defineProps({
 	role: Object
@@ -8,10 +9,13 @@ defineProps({
 </script>
 
 <template>
-	<div class="rounded-md overflow-hidden border border-r-4 border-slate-300">
-		<div class="p-2 bg-slate-100 font-bold border-b">{{ role?.userName }} <span class="text-slate-400 font-thin">({{
-			role?.roleName
-		}})</span>
+	<div class="rounded-md border border-r-4 border-slate-300">
+		<div class="p-2 bg-slate-100 font-bold border-b rounded-t-md">{{ role?.userName }} <span
+				class="text-slate-400 font-thin">
+				<Icon :icon="role.icon" class="inline-block mx-1 h-full w-6" />{{
+					role?.roleName
+				}}
+			</span>
 		</div>
 		<slot></slot>
 	</div>
