23568aed36a54aca87d407b426e1196a8e8d915d
mnr73 <mnrmilad@gmail.com>
2023-07-15 22:32:01 +0330

    import round structure from separate module file

--- src/pages/Mafia/Game.vue
+++ src/pages/Mafia/Game.vue
@@ -15,6 +15,7 @@ import GhaziStory from "@/components/mafia/GhaziStory.vue";
 import ShahrdarFadayiStory from "@/components/mafia/ShahrdarFadayiStory.vue";
 import UserList from "@/components/mafia/UserList.vue";
 import RoundDetails from "@/components/mafia/RoundDetails.vue";
+import roundStructure from "@/modules/mafia/roundStructure";
 
 const data = new mafia();
 const daysBox = ref(null);
@@ -59,72 +60,7 @@ game.roles = data.getPlayers([]);
 //   return role;
 // });
 
-let rounds_structure = {
-  stepNumber: 0,
-  steps: {
-    night: {
-      number: 0,
-      type: "night",
-      active: true,
-      name: "شب",
-      acts: [],
-    },
-    day: {
-      number: 1,
-      type: "day",
-      active: false,
-      name: "روز",
-      acts: [],
-    },
-    vote_1: {
-      number: 2,
-      type: "vote_1",
-      active: false,
-      name: "رای 1",
-      votes: {},
-      acts: [],
-    },
-    defense: {
-      number: 3,
-      type: "defense",
-      active: false,
-      name: "دفاع",
-      acts: [],
-    },
-  },
-};
-
-if (_.find(game.roles, ["class", "ghazi"])) {
-  rounds_structure.steps.ghazi = {
-    number: _.max(_.map(rounds_structure.steps, "number")) + 1,
-    type: "ghazi",
-    active: false,
-    name: "قاضی",
-    acts: [],
-  };
-}
-rounds_structure.steps.vote_2 = {
-  number: _.max(_.map(rounds_structure.steps, "number")) + 1,
-  type: "vote_2",
-  active: false,
-  name: "رای 2",
-  votes: {},
-  acts: [],
-};
-if (
-  _.find(
-    game.roles,
-    (role) => role.class == "shahrdar" || role.class == "fadayi"
-  )
-) {
-  rounds_structure.steps.shahrdar = {
-    number: _.max(_.map(rounds_structure.steps, "number")) + 1,
-    type: "shahrdar",
-    active: false,
-    name: "شهردار و فدایی",
-    acts: [],
-  };
-}
+let rounds_structure = roundStructure(game);
 
 onMounted(() => {
   let savedGame = data.getGame();
