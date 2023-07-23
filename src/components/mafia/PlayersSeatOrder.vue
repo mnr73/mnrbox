687e65cc1474703d113c4cf0e88e1928a9d5392a
mnr73 <mnrmilad@gmail.com>
2023-07-23 22:36:25 +0330

    select first speaker

--- src/components/mafia/PlayersSeatOrder.vue
+++ src/components/mafia/PlayersSeatOrder.vue
@@ -20,12 +20,22 @@ userList.value = _.filter(props.game.selectedRound?.roles, (role) => {
 });
 userList.value = _.orderBy(userList.value, "dayOrder", "asc");
 
-function log(data) {
+function changeList() {
   userList.value = _.map(userList.value, (role, key) => {
     role.dayOrder = key;
     return role;
   });
-  console.log(userList.value);
+}
+
+function shiftList(index) {
+  // let list = userList.value;
+  // let sliced = _.take(list, index);
+  let sliced = _.remove(userList.value, function (n, i) {
+    return i < index;
+  });
+  userList.value = [...userList.value, ...sliced];
+  // userList.value.push(sliced);
+  changeList();
 }
 </script>
 
@@ -48,18 +58,28 @@ function log(data) {
         v-if="userList.length"
         v-model="userList"
         item-key="class"
-        @change="log"
+        @change="changeList"
         class="flex flex-col gap-2"
       >
-        <template #item="{ element }" v-if="userList.length">
-          <div class="bg-slate-50 border rounded-md p-1 flex gap-2">
-            <div>
+        <template #item="{ element, index }" v-if="userList.length">
+          <div
+            class="bg-slate-50 border rounded-md flex gap-2 h-8 items-center"
+          >
+            <div class="p-1">
               <Icon
                 icon="akar-icons:drag-horizontal"
                 class="w-6 h-6 text-slate-400"
               />
             </div>
-            <div>{{ element.userName }}</div>
+            <div class="p-1">{{ index }}</div>
+            <div class="p-1">{{ element.userName }}</div>
+            <div
+              class="ms-auto h-full w-10 flex items-center justify-center rounded-e-md bg-slate-500 text-white cursor-pointer"
+              :class="{ 'bg-sky-500': element == _.first(userList) }"
+              @click="shiftList(index)"
+            >
+              <Icon icon="akar-icons:align-to-top" class="w-5 h-6" />
+            </div>
           </div>
         </template>
       </draggable>
